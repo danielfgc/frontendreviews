4125349ef267e13716051b5e8228cff8a70c218f
danielfgc <daniel.fernandez@campusfp.es>
2023-02-09 11:59:56 +0100

    cors issue fixed

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -7,14 +7,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AuthService {
 
   constructor(private http: HttpClient) { }
-  login(email: String, pass:String){
-    let headers = new HttpHeaders({"Content-Type":"application/json"});
+  login(email: string, pass:string){
+    let headers = new HttpHeaders().set('content-type', 'application/json')
+    .set('Access-Control-Allow-Origin', '*');
     let body = {email:email,password: pass };
     return this.http.post("http://localhost:8082/api/login", body, {headers});
   }
-  register(email: String, pass:String, username:String){
-    let headers = new HttpHeaders({"Content-Type":"application/json"});
-    let body = {username: username,email:email, password: pass};
+  register(username:string,email: string, pass:string ){
+    let headers = new HttpHeaders().set('content-type', 'application/json')
+    .set('Access-Control-Allow-Origin', '*');
+    let body = {username: username, email:email, password: pass};
     return this.http.post("http://localhost:8082/api/register", body, {headers});
   }
   toggleForm(){
